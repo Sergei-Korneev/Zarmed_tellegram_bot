78b33f9fc87fc0fecad125f4a8370da64e727431
Sergei Korneev <s.m.korneev@yandex.ru>
2024-09-20 21:43:58 +0500

    29149

--- instredis.sh
+++ instredis.sh
@@ -1,6 +1,6 @@
-  install lsb-release curl gpg
+ apt install lsb-release curl gpg
 curl -fsSL https://packages.redis.io/gpg |  gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
  chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" |  tee /etc/apt/sources.list.d/redis.list
-  update
-  install redis
+ apt update
+ apt install redis
