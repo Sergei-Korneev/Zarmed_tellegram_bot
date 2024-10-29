8c724c629680a6c0b4b0d92e5ffd59a017446b92
Sergei Korneev <s.m.korneev@yandex.ru>
2024-10-29 23:45:00 +0500

    20260

--- botzm.py
+++ botzm.py
@@ -80,7 +80,7 @@ class ClientState(StatesGroup):
 # Get settings
 async def GetSettings():
        result = http1c.DBRequest('appapi/getSet')
-       print(result)
+       logging.info("Trying to get token. " + str(result))
        if result[0] == 200:
            global bot
            bot = Bot(token=result[1]["TgToken"], default=DefaultBotProperties(parse_mode=ParseMode.HTML))
