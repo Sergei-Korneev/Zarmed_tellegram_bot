111d1962f932aea30c865985e9947037be9116ca
Sergei Korneev <s.m.korneev@yandex.ru>
2024-11-15 19:35:33 +0500

    16908

--- botzm.py
+++ botzm.py
@@ -49,9 +49,9 @@ qreader = QReader()
  
 
 # Credentials
-TOKEN = getenv("BOT_TOKEN")
-bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-#bot = None
+#TOKEN = getenv("BOT_TOKEN")
+#bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = None
 
  
 
@@ -577,19 +577,14 @@ async  def call_handler(message: CallbackQuery, state: FSMContext):
             return
       
        
-    
-
-  
-  
-  
-  
+     
   
  
 # Main function 
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
     
-    #await GetSettings()
+    await GetSettings()
     if bot == None:
         logging.error("Cannot start bot (cannot get Token from server)")
         return
